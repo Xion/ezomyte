1133cb95c102c311cdc57f7655dd0e325e388f34
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2018-01-08 20:54:37 +0100

    A bunch of deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//!
+//! ezomyte
+//! Path of Exile API client
+//!
+
+             extern crate hyper;
+             extern crate hyper_tls;
+             extern crate serde;
+#[macro_use] extern crate serde_derive;
+             extern crate serde_json;
+             extern crate tokio_core;
+
+
+mod util;
+
+
 #[cfg(test)]
 mod tests {
     #[test]
