d9e29fa32887024b7e9cd867a226963774abf017
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2018-01-25 14:26:29 -0800

    Example displaying item names

---
+++ examples/item_names.rs
@@ -0,0 +1,30 @@
+//! Example showing how to use the client to query & display
+//! just the names of publically visible items.
+
+extern crate env_logger;
+extern crate ezomyte;
+extern crate futures;
+extern crate tokio_core;
+
+
+use futures::Stream;
+use tokio_core::reactor::Core;
+
+
+const USER_AGENT: &str = "ezomyte example:item_names";
+
+
+fn main() {
+    env_logger::init();
+
+    let mut core = Core::new().unwrap();
+    let client = ezomyte::Client::new(USER_AGENT, &core.handle());
+    core.run(
+        client.stashes().all().for_each(|stash| {
+            for item in stash.items {
+                println!("{}", item.name.as_ref().unwrap_or(&item.base));
+            }
+            Ok(())
+        })
+    ).unwrap();
+}
